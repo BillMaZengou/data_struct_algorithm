1270ff69611e97785b631ebdebee65f12fb70cc7
Bill Ma <mazengou@gmail.com>
2020-08-02 12:15:43 +0800

    Use Stack implement basic calculation logic

---
+++ python/ch_03/postfixEval.py
@@ -0,0 +1,34 @@
+from basic_functions import Stack
+from infixToPostfix import infixToPostfix
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+
+    return operandStack.pop()
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    elif op == "-":
+        return op1 - op2
+
+def main():
+    print(postfixEval("4 5 6 * +"))
+    print(postfixEval("7 8 + 3 2 + /"))
+
+if __name__ == '__main__':
+    main()
