274371e102444d7c39b1994be1c609830fdc5114
Bill Ma <mazengou@gmail.com>
2020-08-13 20:49:23 +0800

    Unordered List

--- python/ch_03/basic_functions.py
+++ python/ch_03/basic_functions.py
@@ -64,6 +64,74 @@ class Deque(object):
     def size(self):
         return len(self.items)
 
+class Node(object):
+    """docstring for Node."""
+
+    def __init__(self, initdata):
+        super(Node, self).__init__()
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+class UnorderedList(object):
+    """docstring for UnorderedList."""
+
+    def __init__(self):
+        super(UnorderedList, self).__init__()
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def length(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.setNext())
 
 def main():
     """Stack"""
@@ -94,17 +162,33 @@ def main():
     # print(q.size())
 
     """Deque"""
-    d = Deque()
-    print(d.isEmpty())
-    d.addRear(4)
-    d.addRear("dog")
-    d.addFront("cat")
-    d.addFront(True)
-    print(d.size())
-    print(d.isEmpty())
-    d.addRear(8.4)
-    print(d.removeRear())
-    print(d.removeFront())
+    # d = Deque()
+    # print(d.isEmpty())
+    # d.addRear(4)
+    # d.addRear("dog")
+    # d.addFront("cat")
+    # d.addFront(True)
+    # print(d.size())
+    # print(d.isEmpty())
+    # d.addRear(8.4)
+    # print(d.removeRear())
+    # print(d.removeFront())
+
+    """Node (for Linked List)"""
+    # temp = Node(93)
+    # print(temp.getData())
+    # print(temp.getNext())
+
+    """Unordered List"""
+    mylist = UnorderedList()
+    mylist.add(31)
+    mylist.add(77)
+    mylist.add(17)
+    mylist.add(93)
+    mylist.add(31)
+    mylist.add(31)
+    print(mylist.search(17))
+
 
 if __name__ == '__main__':
     main()
